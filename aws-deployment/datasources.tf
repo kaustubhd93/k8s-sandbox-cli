c549b8edf66f140ca88ea72a2391dab407161e8b
kaustubhd93 <kaustubhdesai93@gmail.com>
2024-03-17 23:06:59 +0530

    Use 22.04 ami

--- aws-deployment/datasources.tf
+++ aws-deployment/datasources.tf
@@ -1,10 +1,16 @@
 data "aws_ami" "server_ami" {
   most_recent = true
+  # Canonical account ID
   owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    # values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
